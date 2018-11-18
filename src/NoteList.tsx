64af4b9058b4a23a6c8f4d7fdc69855b53192859
John Reeves <vectorjohn@gmail.com>
2018-11-17 17:35:50 -0800

    a dash of styling

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Note } from './notes/NoteService';
+import './NoteList.css';
 
 export default ({notes}: {notes: Note[]}) =>
-  <dl>
+  <dl className="NoteList">
     {notes.map(note => (<React.Fragment key={note.id}><dt>{note.title}</dt><dd>{note.body}</dd></React.Fragment>))}
   </dl>
